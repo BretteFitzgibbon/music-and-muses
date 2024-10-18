52585f041b46ea5b0d4e8bc1eb984ed5114bb21a
summerliu-usf <kliu49@usfca.edu>
2024-10-18 15:00:35 -0700

    horizontal layout version added

--- src/main/java/com/example/application/views/home/HomeView.java
+++ src/main/java/com/example/application/views/home/HomeView.java
@@ -15,6 +15,7 @@ import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouteAlias;
 import ai.peoplecode.OpenAIConversation;
+
 import java.util.List;
 
 
